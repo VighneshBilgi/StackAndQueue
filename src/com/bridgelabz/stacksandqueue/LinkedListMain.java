2dec65b614ec0bc8d9329f440d7b672a5a819231
VighneshBilgi <vhbilgi@gmail.com>
2022-05-25 19:59:04 +0530

    Added: Welcome to Stack and Queue

---
+++ src/com/bridgelabz/stacksandqueue/LinkedListMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.stacksandqueue;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Stack and Queue");
+    }
+}
